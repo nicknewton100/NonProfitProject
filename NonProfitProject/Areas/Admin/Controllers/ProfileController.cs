d49986852bb079ca93d49772e81a63201e62d2f8
Duean <duean.black@gmail.com>
2021-06-20 18:25:11 -0400

    Adding page to the Admin folder... dashboards and profile page.

---
+++ NonProfitProject/Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NonProfitProject.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
