1d510f023fe8d3b05f488740939b243b02124c0d
Brent Thomason <42630028+thomasonbd@users.noreply.github.com>
2021-06-26 15:05:14 -0400

    Added Calendar but it looks rough. Pushing Commit before further editing

---
+++ NonProfitProject/wwwroot/js/Calendar.js
@@ -0,0 +1,18 @@
+﻿let calendarEl = document.getElementById('calendar');
+
+let calendar = new FullCalendar.Calendar(calendarEl, {
+    initalView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    events: [
+        {
+            title: "Event List",
+            start: "2021-06-26",
+        },
+    ],
+});
+
+calendar.render();
