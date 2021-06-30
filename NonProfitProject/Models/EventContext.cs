9f83dc0461edc936625525851ce0289a6e07794d
Brent Thomason <42630028+thomasonbd@users.noreply.github.com>
2021-06-29 22:48:09 -0400

    Created Event Context but waiting on DateTime2 format fix

---
+++ NonProfitProject/Models/EventContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonProfitProject.Models
+{
+    public class EventContext : DbContext
+    {
+        public EventContext(DbContextOptions<EventContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Event>().HasData(
+            new Event
+            {
+                EventID = 1,
+                EventStartDate = ,
+                EventEndDate = (),
+                EventDescription = ""
+            },
+            new Event
+            {
+                EventID = 2,
+                EventStartDate = ,
+                EventEndDate = (),
+                EventDescription = ""
+            },
+            new Event
+            {
+                EventID = 3,
+                EventStartDate = ,
+                EventEndDate = ,
+                EventDescription = ""
+            }
+            );
+        }
+    }
+}
