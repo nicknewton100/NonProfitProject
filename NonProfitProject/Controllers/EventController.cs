c079dab850de66281f000b6d7e8a0dd01daa8425
Brent Thomason <42630028+thomasonbd@users.noreply.github.com>
2021-06-29 22:20:51 -0400

    update

--- NonProfitProject/Controllers/EventController.cs
+++ NonProfitProject/Controllers/EventController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NonProfitProject.Models;
 
 namespace NonProfitProject.Controllers
 {
@@ -19,3 +20,4 @@ namespace NonProfitProject.Controllers
         }
     }
 }
+
