1388585e27397a7646f35b4528d94b7ffe2523a2
Duean <duean.black@gmail.com>
2021-06-29 23:43:32 -0400

    Events page is updated, but need assistance on writing code in the Events controller.

--- NonProfitProject/Models/Event.cs
+++ NonProfitProject/Models/Event.cs
@@ -8,9 +8,15 @@ namespace NonProfitProject.Models
 {
     public class Event
     {
+
         public int EventID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime EventStartDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime EventEndDate { get; set; }
+        public string EventName { get; set; }
         public string EventDescription { get; set; }
     }
 }
