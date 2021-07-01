c84bd83157f9b99c5d3665067837d3ec6777d09e
Brent Thomason <42630028+thomasonbd@users.noreply.github.com>
2021-06-30 21:50:59 -0400

    Setup EventController - Changed Dashboard2.cshtml to give link to AddEvent.cshtml

---
+++ NonProfitProject/Areas/Admin/Controllers/EventController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NonProfitProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NonProfitProject.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+
+    public class EventController : Controller
+    {
+        public IActionResult AddEvent()
+        {
+            return View();
+        }
+
+    }
+}
