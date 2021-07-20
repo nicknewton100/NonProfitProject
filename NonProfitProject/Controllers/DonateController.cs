04354f5cc42b6261312ea4c2bb23ce3c7209dccb
Nicknewton100 <nicholasnewton27@yahoo.com>
2021-07-20 00:30:05 -0400

    Added the ability to submit a donation while signed in to an account

--- NonProfitProject/Controllers/DonateController.cs
+++ NonProfitProject/Controllers/DonateController.cs
@@ -1,28 +1,155 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NonProfitProject.Code.Security;
 using NonProfitProject.Models;
+using NonProfitProject.Models.Extensions;
+using NonProfitProject.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NonProfitProject.Controllers
 {
     public class DonateController : Controller
     {
+        private NonProfitContext context;
+        public DonateController(NonProfitContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = context.Users.Where(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier)).FirstOrDefault();
+                DonationViewModel model = new DonationViewModel()
+                {
+                    FirstName = user.UserFirstName,
+                    LastName = user.UserLastName,
+                    Email = user.Email,
+                    Phone = user.PhoneNumber,
+                    Addr1 = user.UserAddr1,
+                    Addr2 = user.UserAddr2,
+                    City = user.UserCity,
+                    State = user.UserState,
+                    PostalCode = user.UserPostalCode
+                };
+                return View(model);
+            }
             return View();
         }
         [HttpPost]
-        public IActionResult Donate()
+        public IActionResult Donate(DonationViewModel model)
         {
+            if(model.DonationAmount == 0.00m)
+            {
+                ModelState.AddModelError("DonationAmount", "Please select an amount to donate");
+            }
+            if (ModelState.IsValid)
+            {
+                /*Receipts receipt = new Receipts()
+                {
+                    UserID = (User.Identity.IsAuthenticated) ? User.FindFirstValue(ClaimTypes.NameIdentifier) : context.Users.Where(u => u.UserName == "Anonymous").FirstOrDefault().Id,
+                    Total = model.DonationAmount,
+                    Date = DateTime.UtcNow,
+                    Description = "Donation"
+                };
+                Donations donation = new Donations()
+                {
+                    UserID = (User.Identity.IsAuthenticated) ? User.FindFirstValue(ClaimTypes.NameIdentifier) : context.Users.Where(u => u.UserName == "Anonymous").FirstOrDefault().Id,
+                    DonationAmount = model.DonationAmount,
+                    DonationDate = DateTime.UtcNow
+                };
+                InvoiceDonorInformation invoiceDonorInformation = new InvoiceDonorInformation()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Phone = model.Phone,
+                    Addr1 = model.Addr1,
+                    Addr2 = model.Addr2,
+                    City = model.City,
+                    State = model.State,
+                    PostalCode = model.PostalCode
+                };*/
+                HttpContext.Session.SetObject("DonationInformation", model);
+                return RedirectToAction("Payment");
+            }
             return View("Index");
         }
+        [HttpGet]
         public IActionResult Payment()
         {
+            if(HttpContext.Session.GetObject<DonationViewModel>("DonationInformation") == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Payment(DonationPaymentViewModel model)
+        {
+            var DonationInformation = HttpContext.Session?.GetObject<DonationViewModel>("DonationInformation") ?? null;
+            if (DonationInformation == null)
+            {
+                TempData["SessionTimeout"] = "Session has timed out - Please re-enter Donation Information";
+                return RedirectToAction("Index");
+            }
+            if (ModelState.IsValid)
+            {
+                Receipts receipt = new Receipts()
+                {
+                    UserID = (User.Identity.IsAuthenticated) ? User.FindFirstValue(ClaimTypes.NameIdentifier) : context.Users.Where(u => u.UserName == "Anonymous").FirstOrDefault().Id,
+                    Total = DonationInformation.DonationAmount,
+                    Date = DateTime.UtcNow,
+                    Description = "Donation"
+                };
+                Donations donation = new Donations()
+                {
+                    UserID = (User.Identity.IsAuthenticated) ? User.FindFirstValue(ClaimTypes.NameIdentifier) : context.Users.Where(u => u.UserName == "Anonymous").FirstOrDefault().Id,
+                    DonationAmount = DonationInformation.DonationAmount,
+                    DonationDate = DateTime.UtcNow
+                };
+                InvoiceDonorInformation invoiceDonorInformation = new InvoiceDonorInformation()
+                {
+                    FirstName = DonationInformation.FirstName,
+                    LastName = DonationInformation.LastName,
+                    Email = DonationInformation.Email,
+                    Phone = DonationInformation.Phone,
+                    Addr1 = DonationInformation.Addr1,
+                    Addr2 = DonationInformation.Addr2,
+                    City = DonationInformation.City,
+                    State = DonationInformation.State,
+                    PostalCode = DonationInformation.PostalCode
+                };
+                AesEncryption aes = new AesEncryption();
+                InvoicePayment invoicePayment = new InvoicePayment()
+                {
+                    CardholderName = model.CardholderName,
+                    CardType = model.CardType,
+                    CardNumber = aes.Encrypt(model.CardNumber),
+                    ExpDate = model.ExpDate,
+                    CVV = aes.Encrypt(model.CVV),
+                    Last4Digits = Int32.Parse(model.CardNumber.Substring(model.CardNumber.Length - 4)),
+                    BillingFirstName = model.BillingFirstName,
+                    BillingLastName = model.BillingLastName,
+                    BillingAddr1 = model.BillingAddr1,
+                    BillingAddr2 = model.BillingAddr2,
+                    BillingCity = model.BillingCity,
+                    BillingState = model.BillingState,
+                    BillingPostalCode = model.BillingPostalCode
+                };
+                receipt.Donation = donation;
+                receipt.InvoiceDonorInformation = invoiceDonorInformation;
+                receipt.InvoicePayment = invoicePayment;
+                context.Add(receipt);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
---
+++ NonProfitProject/Models/ViewModels/DonationPaymentViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonProfitProject.Models.ViewModels
+{
+    public class DonationPaymentViewModel
+    {
+        [Required(ErrorMessage = "Please enter Cardholder Name")]
+        public string CardholderName { get; set; }
+        [Required(ErrorMessage = "Please slect Card Type")]
+        public string CardType { get; set; }
+        [Required(ErrorMessage = "Please enter Card number")]
+        public string CardNumber { get; set; }
+        [Required(ErrorMessage = "Please enter Card Expiration Date")]
+        public string ExpDate { get; set; }
+        [Required(ErrorMessage = "Please enter CVV Number")]
+        public string CVV { get; set; }
+
+        //this is used for billing address information
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string BillingFirstName { get; set; }
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string BillingLastName { get; set; }
+        [Required(ErrorMessage = "Please enter Address")]
+        public string BillingAddr1 { get; set; }
+        public string BillingAddr2 { get; set; }
+        [Required(ErrorMessage = "Please enter City")]
+        public string BillingCity { get; set; }
+        [Required(ErrorMessage = "Please select a State")]
+        public string BillingState { get; set; }
+        [Required(ErrorMessage = "Please enter Zip Code")]
+        public int BillingPostalCode { get; set; }
+
+        public bool useHomeAddress { get; set; }
+    }
+}
