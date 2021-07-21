ca078f8483d5638ab377dd2de61dd2071d112b38
Nicknewton100 <nicholasnewton27@yahoo.com>
2021-07-21 11:57:25 -0400

    Added Donation History and Donation Details Notes: Also created an if statement in the donate controller so that Beau would not be getting the emails for donating as of now. Normally, it send receipt information as well as a thank you note. I have to turn it back on before turn in!

---
+++ NonProfitProject/Areas/Admin/Controllers/DonationHistoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NonProfitProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonProfitProject.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class DonationHistoryController : Controller
+    {
+        private NonProfitContext context;
+        public DonationHistoryController(NonProfitContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var receipts = context.Receipts.Include(r => r.Donation).Include(r => r.InvoicePayment).Include(r => r.InvoiceDonorInformation).Include(r => r.User).OrderBy(r => r.Date).ToList();
+            return View(receipts);
+        }
+        public IActionResult Details(int id)
+        {
+            TempData["Action"] = "Details";
+            var donation = context.Receipts.Include(r => r.InvoicePayment).Include(r => r.InvoiceDonorInformation).Include(r => r.Donation).Include(r => r.User).Where(r => r.ReceiptID == id).FirstOrDefault();
+            return View(donation);
+        }
+    }
+}
