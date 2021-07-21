d421281b0d29699fc21fb8dea30c9989f27edd84
Nicknewton100 <nicholasnewton27@yahoo.com>
2021-07-21 15:19:05 -0400

    Added the ability to edit donations as an admin

--- NonProfitProject/Areas/Admin/Controllers/DonationHistoryController.cs
+++ NonProfitProject/Areas/Admin/Controllers/DonationHistoryController.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NonProfitProject.Areas.Admin.Models.ViewModels;
+using NonProfitProject.Code.Security;
 
 namespace NonProfitProject.Areas.Admin.Controllers
 {
@@ -25,9 +27,91 @@ namespace NonProfitProject.Areas.Admin.Controllers
         }
         public IActionResult Details(int id)
         {
-            TempData["Action"] = "Details";
             var donation = context.Receipts.Include(r => r.InvoicePayment).Include(r => r.InvoiceDonorInformation).Include(r => r.Donation).Include(r => r.User).Where(r => r.ReceiptID == id).FirstOrDefault();
+            if (donation == null)
+            {
+                return RedirectToAction("Index");
+            }
             return View(donation);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var receipt = context.Receipts.Include(r => r.InvoicePayment).Include(r => r.InvoiceDonorInformation).Include(r => r.Donation).Include(r => r.User).Where(r => r.ReceiptID == id).FirstOrDefault();
+            if(receipt == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var editDonation = new EditDonationViewModel
+            {
+                ReceiptID = receipt.ReceiptID,
+                Username = receipt.User.UserName,
+                DonationAmount = receipt.Donation.DonationAmount,
+                FirstName = receipt.InvoiceDonorInformation.FirstName,
+                LastName = receipt.InvoiceDonorInformation.LastName,
+                Email = receipt.InvoiceDonorInformation.Email,
+                Phone = receipt.InvoiceDonorInformation.Phone,
+                Addr1 = receipt.InvoiceDonorInformation.Addr1,
+                Addr2 = receipt.InvoiceDonorInformation.Addr2,
+                City = receipt.InvoiceDonorInformation.City,
+                State = receipt.InvoiceDonorInformation.State,
+                PostalCode = receipt.InvoiceDonorInformation.PostalCode
+            };
+            return View(editDonation);
+        }
+        [HttpPost]
+        public IActionResult Edit(EditDonationViewModel model)
+        {
+            var currentReceipt = context.Receipts.Include(r => r.InvoicePayment).Include(r => r.InvoiceDonorInformation).Include(r => r.Donation).Include(r => r.User).Where(r => r.ReceiptID == model.ReceiptID).AsNoTracking().FirstOrDefault();
+            model.Username = currentReceipt.User.UserName;
+            if (!model.isChangingCardInformation)
+            {
+                ModelState.Remove("CardholderName");
+                ModelState.Remove("CardType");
+                ModelState.Remove("CardNumber");
+                ModelState.Remove("ExpDate");
+                ModelState.Remove("CVV");
+                ModelState.Remove("BillingFirstName");
+                ModelState.Remove("BillingLastName");
+                ModelState.Remove("BillingAddr1");
+                ModelState.Remove("BillingAddr2");
+                ModelState.Remove("BillingCity");
+                ModelState.Remove("BillingState");
+                ModelState.Remove("BillingPostalCode");
+            }
+            if (ModelState.IsValid)
+            {
+                //sets receipt information
+                currentReceipt.Total = model.DonationAmount;
+                //sets donation information
+                currentReceipt.Donation.DonationAmount = model.DonationAmount;
+                //personal information
+                currentReceipt.InvoiceDonorInformation.FirstName = model.FirstName;
+                currentReceipt.InvoiceDonorInformation.LastName = model.LastName;
+                currentReceipt.InvoiceDonorInformation.Email = model.Email;
+                currentReceipt.InvoiceDonorInformation.Phone = model.Phone;
+                currentReceipt.InvoiceDonorInformation.Addr1 = model.Addr1;
+                currentReceipt.InvoiceDonorInformation.Addr2 = model.Addr2;
+                currentReceipt.InvoiceDonorInformation.City = model.City;
+                currentReceipt.InvoiceDonorInformation.State = model.State;
+                currentReceipt.InvoiceDonorInformation.PostalCode = model.PostalCode;
+
+                //billing information if it was checked to be changed
+                if (model.isChangingCardInformation)
+                {
+                    AesEncryption aes = new AesEncryption();
+                    currentReceipt.InvoicePayment.CardholderName = model.CardholderName;
+                    currentReceipt.InvoicePayment.CardType = model.CardType;
+                    currentReceipt.InvoicePayment.CardNumber = aes.Encrypt(model.CardNumber);
+                    currentReceipt.InvoicePayment.ExpDate = model.ExpDate;
+                    currentReceipt.InvoicePayment.CVV = model.CVV;
+                    currentReceipt.InvoicePayment.Last4Digits = Int32.Parse(model.CardNumber.Substring(model.CardNumber.Length - 4));
+                }
+                context.Receipts.Update(currentReceipt);
+                context.SaveChanges();
+                return RedirectToAction("Details");
+            }
+            return View();
         }
     }
 }
---
+++ NonProfitProject/Areas/Admin/Models/ViewModels/EditDonationViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonProfitProject.Areas.Admin.Models.ViewModels
+{
+    public class EditDonationViewModel
+    {
+        //receipt information
+        public int ReceiptID { get; set; }
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please select donation amount")]
+        public decimal DonationAmount { get; set; }
+
+        //personal information
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please enter Email Address")]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please enter Address")]
+        public string Addr1 { get; set; }
+        public string Addr2 { get; set; }
+        [Required(ErrorMessage = "Please enter City")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please enter State")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "Please enter Postal Code")]
+        public int PostalCode { get; set; }
+
+        public bool isChangingCardInformation { get; set; }
+
+        //card information
+        [Required(ErrorMessage = "Please enter Cardholder name")]
+        public string CardholderName { get; set; }
+        [Required(ErrorMessage = "Please select Card Type")]
+        public string CardType { get; set; }
+        [Required(ErrorMessage = "Please enter Card Number")]
+        public string CardNumber { get; set; }
+        [Required(ErrorMessage = "Please enter Expiration Date")]
+        public string ExpDate { get; set; }
+        [Required(ErrorMessage = "Please enter CVV")]
+        public string CVV { get; set; }
+
+        //billing information
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string BillingFirstName { get; set; }
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string BillingLastName { get; set; }
+        [Required(ErrorMessage = "Please enter Address")]
+        public string BillingAddr1 { get; set; }
+        public string BillingAddr2 { get; set; }
+        [Required(ErrorMessage = "Please enter City")]
+        public string BillingCity { get; set; }
+        [Required(ErrorMessage = "Please enter State")]
+        public string BillingState { get; set; }
+        [Required(ErrorMessage = "Please enter Postal Code")]
+        public int? BillingPostalCode { get; set; }
+    }
+}
