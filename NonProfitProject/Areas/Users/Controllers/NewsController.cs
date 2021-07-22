32ad4e90a7186343ce9198ab26a9d621113c98a2
Duean <duean.black@gmail.com>
2021-07-21 20:14:07 -0400

    ...

---
+++ NonProfitProject/Areas/Users/Controllers/NewsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NonProfitProject.Areas.Users.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Area("Users")]
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
