b7a4f3cfc69ad8659f1603280ca5222f8271b090
Nicknewton100 <nicholasnewton27@yahoo.com>
2021-07-26 21:35:16 -0400

    added Donor Contact Information Report

--- NonProfitProject/Areas/Admin/Controllers/BackupController.cs
+++ NonProfitProject/Areas/Admin/Controllers/BackupController.cs
@@ -18,6 +18,7 @@ using System.Data.Common;
 using Microsoft.EntityFrameworkCore;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Identity;
 
 namespace NonProfitProject.Areas.Admin.Controllers
 {
@@ -27,11 +28,13 @@ namespace NonProfitProject.Areas.Admin.Controllers
     {
         private IWebHostEnvironment webHostEnvironment;
         private NonProfitContext context;
-        public BackupController(IWebHostEnvironment webHostEnvironment, NonProfitContext context)
+        private UserManager<NonProfitProject.Models.User> userManager;
+        public BackupController(IWebHostEnvironment webHostEnvironment, NonProfitContext context, UserManager<NonProfitProject.Models.User> userManager)
         {
             this.webHostEnvironment = webHostEnvironment;
             this.context = context;
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            this.userManager = userManager;
         }
         
         public IActionResult Index()
@@ -64,7 +67,7 @@ namespace NonProfitProject.Areas.Admin.Controllers
                 options.AppendToFile = true;
                 options.Indexes = true;
                 options.WithDependencies = true;
-                List<string> tables = new List<string> { "__EFMigrationsHistory", "AspNetRoleClaims", "AspNetRoles", "AspNetUserClaims", "AspNetUserLogins", "AspNetUserRoles", "AspNetUsers", "AspNetUserTokens", "CommitteeMembers", "Committees", "Donations", "Employees", "Events", "InvoiceDonorInformation", "InvoicePayments", "MembershipDues", "News", "Receipts", "SavedPayments" };
+                List<string> tables = new List<string> { "__EFMigrationsHistory", "AspNetRoleClaims", "AspNetRoles", "AspNetUserClaims", "AspNetUserLogins", "AspNetUserRoles", "AspNetUsers", "AspNetUserTokens", "CommitteeMembers", "Committees", "Donations", "Employees", "Events", "InvoiceDonorInformation", "InvoicePayments", "MembershipDues", "News", "Receipts", "SavedPayments", "MembershipType" };
                 foreach (var tbl in tables)
                 {
                     dbs.Tables[tbl].EnumScript(options);
@@ -98,12 +101,34 @@ namespace NonProfitProject.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Print()
+        public IActionResult EmployeeReport()
         {
-            var employee = context.Employees.Include(e => e.User).ToList();
+            var employee = context.Employees.Include(e => e.User).OrderBy(e => e.User.UserLastName + ", " + e.User.UserFirstName).ToList();
             DataTable dt = new DataTable();
-            dt = getData(employee);
-
+            dt.Columns.Add("Number");
+            dt.Columns.Add("EmpPosition");
+            dt.Columns.Add("EmpName");
+            dt.Columns.Add("EmpEmail");
+            dt.Columns.Add("EmpPhone");
+            dt.Columns.Add("EmpAddress");
+            dt.Columns.Add("EmpHireDate");
+            dt.Columns.Add("EmpReleaseDate");
+            DataRow row;
+            int number = 0;
+            foreach (var i in employee)
+            {
+                number += 1;
+                row = dt.NewRow();
+                row["Number"] = number;
+                row["EmpPosition"] = i.Position;
+                row["EmpName"] = i.User.UserLastName + ", " + i.User.UserFirstName;
+                row["EmpEmail"] = i.User.Email;
+                row["EmpPhone"] = i.User?.PhoneNumber ?? "Null";
+                row["EmpAddress"] = String.Format("{0}, {1}{2}, {3} {4}", i.User.UserAddr1.Replace(".", "").TrimEnd(), i.User.UserAddr2 == "" ? i.User.UserAddr2 + ", " : "", i.User.UserCity, i.User.UserState, i.User.UserPostalCode);
+                row["EmpHireDate"] = i.HireDate.ToShortDateString();
+                row["EmpReleaseDate"] = i.ReleaseDate.HasValue ? i.ReleaseDate.Value.ToShortDateString() : "Null";
+                dt.Rows.Add(row);
+            }
             string mimetype = "";
             int extension = 1;
             var path = $"{webHostEnvironment.WebRootPath}\\Reports\\rptEmployeeContact.rdlc";
@@ -117,38 +142,43 @@ namespace NonProfitProject.Areas.Admin.Controllers
             return File(result.MainStream, "application/pdf");
         }
 
-        public DataTable getData(List<Employees> context)
+        [HttpPost]
+        public async Task<IActionResult> DonorReport()
         {
-            var dt = new DataTable();
+            var users = await userManager.GetUsersInRoleAsync("User");
+            var donor = context.Users.Where(u => users.Contains(u)).OrderBy(u => u.UserLastName + ", " + u.UserFirstName).ToList();
+            DataTable dt = new DataTable();
             dt.Columns.Add("Number");
-            dt.Columns.Add("EmpPosition");
-            dt.Columns.Add("EmpName");
-            dt.Columns.Add("EmpEmail");
-            dt.Columns.Add("EmpPhone");
-            dt.Columns.Add("EmpAddress");
-            dt.Columns.Add("EmpHireDate");
-            dt.Columns.Add("EmpReleaseDate");
+            dt.Columns.Add("DonorName");
+            dt.Columns.Add("DonorEmail");
+            dt.Columns.Add("DonorPhone");
+            dt.Columns.Add("DonorAddress");
+            dt.Columns.Add("DonorJoinDate");
             DataRow row;
             int number = 0;
-            foreach(var i in context)
+            foreach (var i in donor)
             {
                 number += 1;
                 row = dt.NewRow();
                 row["Number"] = number;
-                row["EmpPosition"] = i.Position;
-                row["EmpName"] = i.User.UserFirstName + " " + i.User.UserLastName;
-                row["EmpEmail"] = i.User.Email;
-                row["EmpPhone"] = i.User?.PhoneNumber ?? "Null";
-                row["EmpAddress"] = String.Format("{0}, {1}{2}, {3} {4}", i.User.UserAddr1.Replace(".", "").TrimEnd(), i.User.UserAddr2 == "" ? i.User.UserAddr2 + ", " : "", i.User.UserCity, i.User.UserState, i.User.UserPostalCode);
-                row["EmpHireDate"] = i.HireDate.ToShortDateString();
-                row["EmpReleaseDate"] = i.ReleaseDate.HasValue ? i.ReleaseDate.Value.ToShortDateString() : "Null";
+                row["DonorName"] = i.UserLastName + ", " + i.UserFirstName;
+                row["DonorEmail"] = i.Email;
+                row["DonorPhone"] = i.PhoneNumber == null ? "Null": i.PhoneNumber;
+                row["DonorAddress"] = String.Format("{0}, {1}{2}, {3} {4}", i.UserAddr1.Replace(".", "").TrimEnd(), i.UserAddr2 == "" ? i.UserAddr2 + ", " : "", i.UserCity, i.UserState, i.UserPostalCode);
+                row["DonorJoinDate"] = i.UserCreationDate.ToShortDateString();
                 dt.Rows.Add(row);
             }
-            return dt;
-        }
-        /*public DataTable getUser(List<NonProfitProject.Models.User> context)
-        {
+            string mimetype = "";
+            int extension = 1;
+            var path = $"{webHostEnvironment.WebRootPath}\\Reports\\rptDonorContact.rdlc";
 
-        }*/
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("prm", "RDLC Report");
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("dsDonorInformation", dt);
+            //localReport.AddDataSource("dsUsers",context)
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
+            return File(result.MainStream, "application/pdf");
+        }
     }
 }
