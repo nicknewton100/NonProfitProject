1889545bb643a20462cea8357b3277a7d50822fd
Nicknewton100 <nicholasnewton27@yahoo.com>
2021-08-01 20:49:55 -0400

    added employee news and added event details on click for event calendar

--- NonProfitProject/Areas/Employee/Controllers/NewsController.cs
+++ NonProfitProject/Areas/Employee/Controllers/NewsController.cs
@@ -1,19 +1,125 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using NonProfitProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using NonProfitProject.Areas.Employee.Controllers.DefaultControllers;
+using NonProfitProject.Areas.Employee.Data;
 
 namespace NonProfitProject.Areas.Employee.Controllers
 {
     [Authorize(Roles = "Employee")]
     [Area("Employee")]
-    public class NewsController : Controller
+    public class NewsController : DefaultEmployeeController
     {
+        private NonProfitContext context;
+        public NewsController(NonProfitContext context) : base(context)
+        {
+            this.context = context;
+        }
+        public bool isNewsCommittee()
+        {
+            var name = CommitteeStatus.GetName(context, User.FindFirstValue(ClaimTypes.NameIdentifier));
+            if (name == "News Committee")
+            {
+                return true;
+            }
+            return false;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            if (!isNewsCommittee())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var news = context.News.ToList();
+            return View(news);
+        }
+        [HttpGet]
+        public IActionResult AddNews()
+        {
+            if (!isNewsCommittee())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Action = "Add";
+            return View("EditNews", new News());
+        }
+
+        [HttpGet]
+        public IActionResult EditNews(int id)
+        {
+            if (!isNewsCommittee())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Action = "Edit";
+            var news = context.News.Find(id);
+            if (news == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        [HttpPost]
+        public IActionResult EditNews(News model)
+        {
+            if (!isNewsCommittee())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if (ModelState.IsValid)
+            {
+                string addOrEdit;
+                if (model.NewsID == 0 || context.News.AsNoTracking().Where(n => n.NewsID == model.NewsID).FirstOrDefault() == null)
+                {
+                    var user = context.Users.Where(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier)).FirstOrDefault();
+                    model.NewsID = 0;
+                    model.NewsPublishDate = DateTime.UtcNow;
+                    model.NewsLastUpdated = DateTime.UtcNow;
+                    model.CreatedBy = user.UserFirstName + " " + user.UserLastName;
+                    context.News.Add(model);
+                    addOrEdit = "added";
+                }
+                else
+                {
+                    var news = context.News.Where(n => n.NewsID == model.NewsID).FirstOrDefault();
+                    news.NewsLastUpdated = DateTime.UtcNow;
+                    news.NewsTitle = model.NewsTitle;
+                    news.NewsHeader = model.NewsHeader;
+                    news.NewsBody = model.NewsBody;
+                    context.News.Update(news);
+                    addOrEdit = "updated";
+                }
+
+                context.SaveChanges();
+                TempData["EventChanges"] = String.Format("The Event \"{0}\" has been {1}.", model.NewsTitle, addOrEdit);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                ViewBag.Action = (model.NewsID == 0) ? "Add" : "Edit";
+                return View(model);
+            }
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            if (!isNewsCommittee())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var news = context.News.Find(id);
+            context.News.Remove(news);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- NonProfitProject/wwwroot/js/Calendar.js
+++ NonProfitProject/wwwroot/js/Calendar.js
@@ -1,4 +1,5 @@
-﻿let calendarEl = document.getElementById('calendar');
+﻿
+let calendarEl = document.getElementById('calendar');
 
 let calendar = new FullCalendar.Calendar(calendarEl, {
     initalView: 'dayGridMonth',
@@ -7,16 +8,46 @@ let calendar = new FullCalendar.Calendar(calendarEl, {
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
-    events: []
+    eventClick: function (info) {
+        var dateString;
+        
+        if (info.event.start.toDateString() == info.event.end.toDateString()) {
+            dateString = `${moment(info.event.start).format('LL')}    ${moment(info.event.start).format('LT')} - ${moment(info.event.end).format('LT')}`
+        }
+        else{
+            dateString = `${moment(info.event.start).format('LLLL')} - ${moment(info.event.end).format('LLLL')}`
+        }
+        console.log(dateString);
+        Swal.fire({
+            title: `<h1 class:"bg-dark;" style="text-align:center;">${info.event.title}</h1>`,
+            html: `<div style="font-size:medium;"><h4 style="margin-bottom:10px;text-align:left;">Details</h4><p style="text-align:left;"><i class="fas fa-clock" style="margin-right:1em;"></i>${dateString}</p><p style="text-align:left;"><i class="fas fa-map-marker" style="margin-right:1em;"></i>${info.event.extendedProps.address}</p><br /><p style="text-align:left;">${info.event.extendedProps.details}</p></div>`,
+            width: '700',
+            showConfirmButton: false,
+            showCloseButton: true,
+            
+        })
+    },
 });
 
 //references for function below: https://stackoverflow.com/questions/16361364/accessing-mvcs-model-property-from-javascript
 function setCalendar(e) {
     var model = JSON.parse(e);
     for (var i = 0; i < model.length; i++) {
-        model[i].eventStartDate = model[i].eventStartDate.substring(0, model[i].eventStartDate.length - 9);
-        model[i].eventEndDate = model[i].eventEndDate.substring(0, model[i].eventEndDate.length - 9);
-        calendar.addEvent({ title: model[i].eventName, start: model[i].eventStartDate, end: model[i].eventEndDate });
+        var address;
+        if (model[i].eventAddr2 == undefined) {
+            address = model[i].eventAddr1 + ", " + model[i].eventCity + ", " + model[i].eventState + " " + model[i].eventPostalCode;
+        }
+        else {
+            address = model[i].eventAddr1 + " " + model[i].eventAdd2 + ", " + model[i].eventCity + ", " + model[i].eventState + " " + model[i].eventPostalCode;
+        }
+        calendar.addEvent(
+            {
+                title: model[i].eventName,
+                start: model[i].eventStartDate,
+                end: model[i].eventEndDate,
+                details: model[i].eventDescription,
+                address: address
+            });
     }
 }
     calendar.render();
